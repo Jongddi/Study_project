3015b70b02aea57a1c387443b239b1e89ec44d79
Jongddi <75599079+Jongddi@users.noreply.github.com>
2021-03-15 12:55:09 +0900

    그저 룰렛을 돌리는 게임

---
+++ 2D게임프로그래밍/lullet/New Unity Project/Assets/Script/swing.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swing : MonoBehaviour
+{
+    float swingSpeed = 0;
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+		{
+            this.swingSpeed = 20;
+		}
+
+        transform.Rotate(0, 0, swingSpeed);
+
+        swingSpeed *= 0.98f;
+    }
+}
