3b48b3f1ac1097df85d4941d6277cedb5724f5ba
Jongddi <75599079+Jongddi@users.noreply.github.com>
2021-03-31 14:47:54 +0900

    떨어지는 화살을 피하는 게임

---
+++ 2D게임프로그래밍/CatEscape/Assets/Scripts/Playercontroller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour
+{
+    public void LButtonDown()
+    {
+        transform.Translate(-1, 0, 0);
+    }
+
+    public void RButtonDown()
+    {
+        transform.Translate(1, 0, 0);
+    }
+}
