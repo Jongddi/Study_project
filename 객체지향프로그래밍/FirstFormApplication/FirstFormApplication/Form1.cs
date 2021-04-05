15a878fd14ca8caa4a2c6362bfef0071ee94178b
Jongddi <75599079+Jongddi@users.noreply.github.com>
2021-04-05 14:34:26 +0900

    윈도폼으로 버튼 만들기

---
+++ 객체지향프로그래밍/FirstFormApplication/FirstFormApplication/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstFormApplication
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+			myButton.Text = "코드에서 변경!";
+			int width = 100;
+			int height = 23;
+			int margin = 6;
+			myButton.Width = 100;
+
+			for (int i=1; i<5; i++)
+			{
+				Button button = new Button();
+				Controls.Add(button);
+				button.Location = new Point(margin, (height + margin) * (i + 1) + margin);
+				button.Text = "동적 생성" + i + "번째";
+				button.Width = width;
+				button.Height = height;
+			}
+
+		}
+
+	}
+}
