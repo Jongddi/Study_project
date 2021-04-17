6241ce353383858f5cb2fa73504a454789949d2a
Jongddi <75599079+Jongddi@users.noreply.github.com>
2021-04-17 18:17:11 +0900

    윈도폼

---
+++ 객체지향프로그래밍/StripForm/StripForm/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace StripForm
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.menuStrip = new System.Windows.Forms.MenuStrip();
+			this.MenuFile = new System.Windows.Forms.ToolStripMenuItem();
+			this.MenuFileOpen = new System.Windows.Forms.ToolStripMenuItem();
+			this.MenuFileSave = new System.Windows.Forms.ToolStripMenuItem();
+			this.MenuEdit = new System.Windows.Forms.ToolStripMenuItem();
+			this.statusStrip = new System.Windows.Forms.StatusStrip();
+			this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+			this.menuStrip.SuspendLayout();
+			this.statusStrip.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip
+			// 
+			this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuFile,
+            this.MenuEdit});
+			this.menuStrip.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip.Name = "menuStrip";
+			this.menuStrip.Size = new System.Drawing.Size(800, 24);
+			this.menuStrip.TabIndex = 0;
+			this.menuStrip.Text = "menuStrip1";
+			// 
+			// MenuFile
+			// 
+			this.MenuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuFileOpen,
+            this.MenuFileSave});
+			this.MenuFile.Name = "MenuFile";
+			this.MenuFile.Size = new System.Drawing.Size(43, 20);
+			this.MenuFile.Text = "파일";
+			// 
+			// MenuFileOpen
+			// 
+			this.MenuFileOpen.Name = "MenuFileOpen";
+			this.MenuFileOpen.Size = new System.Drawing.Size(98, 22);
+			this.MenuFileOpen.Text = "열기";
+			this.MenuFileOpen.Click += new System.EventHandler(this.MenuFileOpen_Click);
+			// 
+			// MenuFileSave
+			// 
+			this.MenuFileSave.Name = "MenuFileSave";
+			this.MenuFileSave.Size = new System.Drawing.Size(98, 22);
+			this.MenuFileSave.Text = "저장";
+			// 
+			// MenuEdit
+			// 
+			this.MenuEdit.Name = "MenuEdit";
+			this.MenuEdit.Size = new System.Drawing.Size(43, 20);
+			this.MenuEdit.Text = "편집";
+			// 
+			// statusStrip
+			// 
+			this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1});
+			this.statusStrip.Location = new System.Drawing.Point(0, 428);
+			this.statusStrip.Name = "statusStrip";
+			this.statusStrip.Size = new System.Drawing.Size(800, 22);
+			this.statusStrip.TabIndex = 1;
+			this.statusStrip.Text = "statusStrip1";
+			// 
+			// toolStripProgressBar1
+			// 
+			this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+			this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.statusStrip);
+			this.Controls.Add(this.menuStrip);
+			this.MainMenuStrip = this.menuStrip;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.menuStrip.ResumeLayout(false);
+			this.menuStrip.PerformLayout();
+			this.statusStrip.ResumeLayout(false);
+			this.statusStrip.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip;
+		private System.Windows.Forms.ToolStripMenuItem MenuFile;
+		private System.Windows.Forms.ToolStripMenuItem MenuFileOpen;
+		private System.Windows.Forms.ToolStripMenuItem MenuFileSave;
+		private System.Windows.Forms.ToolStripMenuItem MenuEdit;
+		private System.Windows.Forms.StatusStrip statusStrip;
+		private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+	}
+}
+
