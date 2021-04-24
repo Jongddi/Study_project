f92251f3ca0eacd9e1e5da51486d8e601bdc824d
Jongddi <75599079+Jongddi@users.noreply.github.com>
2021-04-24 22:26:22 +0900

    플래피 버드

---
+++ 2D게임프로그래밍/flappybird/Assets/Scripts/Bird.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    private bool isDead = false;
+
+    private Rigidbody2D rigidbody2D;
+    private Animator animator;
+
+    public float upForce = 200f;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(isDead==false)
+		{
+            if(Input.GetMouseButtonDown(0))
+			{
+                rigidbody2D.velocity = Vector2.zero;
+                rigidbody2D.AddForce(new Vector2(0, upForce));
+                animator.SetTrigger("Fly");
+            }
+		}
+    }
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+        isDead = true;
+        animator.SetTrigger("Dead");
+        GameControl.instance.BirdDead();
+    }
+}
