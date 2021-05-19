d180df40538902a16eb9e1203aada201b997e2b7
Jongddi <75599079+Jongddi@users.noreply.github.com>
2021-05-19 16:11:49 +0900

    객체지향 도서관리 프로그램 1차

---
+++ 객체지향프로그래밍/BookManager/BookManager/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookManager
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+            InitializeComponent();
+            Text = "도서관 관리";
+
+            // 라벨 설정
+            label5.Text = DataManager.Books.Count.ToString();
+            label6.Text = DataManager.Users.Count.ToString();
+            label7.Text = DataManager.Books.Where((x) => x.isBorrowed).Count().ToString();
+            label8.Text = DataManager.Books.Where((x) => 
+			{
+                return x.isBorrowed && x.BorrowedAt.AddDays(7) > DateTime.Now;
+            }).Count().ToString();
+
+			// 데이터 그리드 설정
+			dataGridView1.DataSource = DataManager.Books;
+			dataGridView2.DataSource = DataManager.Users;
+			dataGridView1.CurrentCellChanged += DataGridView1_CurrentCellChanged;
+			dataGridView2.CurrentCellChanged += DataGridView2_CurrentCellChanged;
+		}
+
+		private void DataGridView1_CurrentCellChanged(object sender, EventArgs e)
+		{
+			try
+			{
+				if(dataGridView1.CurrentRow != null)
+				{
+					// 그리드의 셀이 선택되면 텍스트박스에 글자 지정
+					Book book = dataGridView1.CurrentRow.DataBoundItem as Book;
+					textBox1.Text = book.Isbn;
+					textBox2.Text = book.Name;
+				}
+			}
+			catch (Exception exception)
+			{
+
+			}
+		}
+
+		private void DataGridView2_CurrentCellChanged(object sender, EventArgs e)
+		{
+			try
+			{
+				if (dataGridView2.CurrentRow != null)
+				{
+					// 그리드의 셀이 선택되면 텍스트박스에 글자 지정
+					User book = dataGridView2.CurrentRow.DataBoundItem as User;
+					textBox3.Text = book.Id.ToString();
+				}
+			}
+			catch (Exception exception)
+			{
+
+			}
+		}
+
+		private void dataGridView1_CurrentCellChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void dataGridView2_CurrentCellChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void toolStripMenuItem1_Click(object sender, EventArgs e)
+		{
+			new Form2().ShowDialog();
+		}
+
+		private void toolStripMenuItem2_Click(object sender, EventArgs e)
+		{
+			new Form3().ShowDialog();
+		}
+	}
+}
