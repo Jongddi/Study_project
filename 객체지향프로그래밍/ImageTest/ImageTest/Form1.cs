df99017ca63ebe4c5e4a7164da253038b2545879
Jongddi <75599079+Jongddi@users.noreply.github.com>
2021-05-29 13:44:50 +0900

    이미지 불러오는 프로그램 만들기

---
+++ 객체지향프로그래밍/ImageTest/ImageTest/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageTest
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Paint(object sender, PaintEventArgs e)
+		{
+			Image newImage = Image.FromFile("../../../Image/사과유.jpg");
+
+			Point pt = new Point(0, 0);
+			e.Graphics.DrawImage(newImage, pt);
+		}
+	}
+}
